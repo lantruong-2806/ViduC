1e1f6c350c241c00b54b5c82832ea9408f4da735
lantruong-2806 <tieuviemtu@gmail.com>
2021-05-23 17:01:12 +0700

    ví dụ về define macro

---
+++ BTVD_DEFINE_MACRO/BTVD_DEFINE_MACRO/main.c
@@ -0,0 +1,30 @@
+//#include<stdio.h>
+//
+//#define MAX(x,y) (x > y) ? x : y
+//#define COUT(s) printf("%s.\r\n",s)
+//
+//int main() {
+//	int a = 2, b = 6, c = 10;
+//	COUT("a");
+//	COUT("b");
+//	printf("so lon nhat trong a, b la: %d\r\n", MAX(a, b));
+//	printf("so lon nhat trong a, b, c la: %d\r\n", MAX((a,b),c));
+//}
+#pragma warning(disable : 4996)
+#include <stdio.h>
+#include <string.h>
+
+struct Books
+{
+	char  tieude[50];
+	char  tacgia[50];
+	char  chude[100];
+	int   id;
+} book2;
+
+int main()
+{
+	struct Books book1;
+	book1.
+	return 0;
+}
